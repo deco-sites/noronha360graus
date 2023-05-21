3507d519b9e8060ed169a21c4a9255eca35dbc4a
Luiz Otávio L. Falcão <39093175+llfalcao@users.noreply.github.com>
2023-05-21 11:54:35 -0300

    Add shortcuts section

--- live.gen.ts
+++ live.gen.ts
@@ -31,8 +31,9 @@ import * as $$$$$$$$10 from "./sections/Mosaic.tsx";
 import * as $$$$$$$$11 from "./sections/ProductDetails.tsx";
 import * as $$$$$$$$12 from "./sections/ProductShelf.tsx";
 import * as $$$$$$$$13 from "./sections/SearchResult.tsx";
-import * as $$$$$$$$14 from "./sections/WhatsApp.tsx";
-import * as $$$$$$$$15 from "./sections/WishlistGallery.tsx";
+import * as $$$$$$$$14 from "./sections/Shortcuts.tsx";
+import * as $$$$$$$$15 from "./sections/WhatsApp.tsx";
+import * as $$$$$$$$16 from "./sections/WishlistGallery.tsx";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
 import * as $live_invoke from "$live/routes/live/invoke/index.ts";
@@ -186,8 +187,9 @@ const manifest = {
     "deco-sites/noronha360graus/sections/ProductDetails.tsx": $$$$$$$$11,
     "deco-sites/noronha360graus/sections/ProductShelf.tsx": $$$$$$$$12,
     "deco-sites/noronha360graus/sections/SearchResult.tsx": $$$$$$$$13,
-    "deco-sites/noronha360graus/sections/WhatsApp.tsx": $$$$$$$$14,
-    "deco-sites/noronha360graus/sections/WishlistGallery.tsx": $$$$$$$$15,
+    "deco-sites/noronha360graus/sections/Shortcuts.tsx": $$$$$$$$14,
+    "deco-sites/noronha360graus/sections/WhatsApp.tsx": $$$$$$$$15,
+    "deco-sites/noronha360graus/sections/WishlistGallery.tsx": $$$$$$$$16,
     "deco-sites/std/sections/Analytics.tsx": i2$$$$3,
     "deco-sites/std/sections/configButterCMS.global.tsx": i2$$$$4,
     "deco-sites/std/sections/configOCC.global.tsx": i2$$$$5,
